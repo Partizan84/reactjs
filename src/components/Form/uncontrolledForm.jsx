74595b46a9b01703b47b09debb93903d6cfd3f7f
Partizan84 <knazarov84@gmail.com>
2021-03-10 00:59:23 +0500

    Обновление и сохранение.

--- src/components/Form/uncontrolledForm.jsx
+++ src/components/Form/uncontrolledForm.jsx
@@ -3,7 +3,7 @@ import { Component, createRef } from 'react';
 class UncontrolledForm extends Component {
     userRef = createRef(); // Стуктура являющаяся объектом, у которого есть ключ current,
     // дефолтное значение имеет null - {current:null}
-    messageRef = createRef();
+    messageRef = createRef(); // Ref работает только с классом, в хуки используется useRef()
 
     handleSend = () => {
         const userName = this.userRef.current.value;
