b32351369a3124c0f99d8c7abd2b1fc63ba9b502
Partizan84 <knazarov84@gmail.com>
2021-03-21 23:32:15 +0500

    Обновление.

--- src/components/ChatList/ChatList.jsx
+++ src/components/ChatList/ChatList.jsx
@@ -2,21 +2,21 @@ import { List, ListItem, ListItemText, ListItemIcon } from '@material-ui/core';
 import Send from '@material-ui/icons/Send';
 import './ChatList.css';
 
-const chats = ['Chat 1', 'Chat 2', 'Chat 3', 'Chat4'];
+const chats = ['Chat 1', 'Chat 2', 'Chat 3', 'Chat 4'];
 
 const ChatList = () => {
     return (
         <div className='chat-list'>
             <List>
-                {chats.map((chat, index) => (
-                    //<Link key={index} to={`/chat/${index}`}>
+                {chats.map((chat) => (
+
                     <ListItem button>
                         <ListItemIcon>
                             <Send />
                         </ListItemIcon>
                         <ListItemText primary={chat} />
                     </ListItem>
-                    //</Link>
+
                 ))}
             </List>
         </div>
